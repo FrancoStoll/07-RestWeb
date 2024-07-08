a3c7447cded302ea4707cbc872c73eb2fc95f904
FrancoStoll <francostoll@gmail.com>
2024-07-07 22:21:48 -0300

    fin-16

---
+++ src/domain/use-cases/todo/get-todo.ts
@@ -0,0 +1,26 @@
+
+import { TodoEntity } from "../../entities/todo.entity";
+import { TodoRepository } from "../../repositories/todo.repository";
+
+
+export interface GetTodoByIdUseCase {
+
+    execute(id: number): Promise<TodoEntity>
+
+
+
+
+}
+export class GetTodo implements GetTodoByIdUseCase {
+
+
+    constructor(
+        private readonly repository: TodoRepository,
+    ) { }
+
+
+    execute(id: number): Promise<TodoEntity> {
+
+        return this.repository.findById(id);
+    }
+}
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -2,6 +2,8 @@
 
 import { Router } from "express";
 import { TodosController } from "./controller";
+import { TodoRepositoryImpl } from "../../infrastructure/repository/todo.repository.impl";
+import { TodoDataSourceImpl } from "../../infrastructure/datasource/todo.datasource.impl";
 
 
 
@@ -11,7 +13,9 @@ export class TodoRoutes {
 
     static get routes(): Router {
         const router = Router();
-        const todoController = new TodosController();
+
+        const todoRepository = new TodoRepositoryImpl(new TodoDataSourceImpl)
+        const todoController = new TodosController(todoRepository);
 
         router.get('/', todoController.getTodos);
         router.get('/:id', todoController.getTodoById)
